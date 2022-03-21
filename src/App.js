9f29784535f33962650a868c00d9d09fb5ab6b3f
vipin v v <vipinvv555@gmail.com>
2022-03-22 01:04:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -219,7 +219,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/counter" element={<Counter/>}/>
-        <Route path="/details/:id" element={<UserDetails/>}/>
+        <Route path="/details/:userId" element={<UserDetails/>}/>
       
 
     </Routes>
--- src/Pages/UserDetails.js
+++ src/Pages/UserDetails.js
@@ -1,14 +1,60 @@
-import React, { useState } from 'react';
-import {useParams} from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { MdOutlineArrowBack } from "react-icons/md";
+import { Button } from "react-bootstrap";
 
 function UserDetails() {
-    const {id} = useParams();
-    console.log('USER-DETAILS-ID', id);
-    // const[website, setWebsite] = useState('');
+  const { userId } = useParams();
+  console.log("USER-DETAILS-ID", userId);
+  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  var final = users.filter((item) => {
+    console.log("itemmm", item);
+    return item.id == userId;
+  });
+
+  console.log("****USERrrr*****", final);
 
   return (
-    <div>Website: {id}</div>
-  )
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="">
+            <Button variant="primary" style={{ width: "3rem" }}>
+              <Link to="/about">
+                <MdOutlineArrowBack
+                  className="text-center"
+                  style={{ fontSize: "30px", color: "white" }}
+                />
+              </Link>
+            </Button>
+          </div>
+        </div>
+          <br />
+        <div className="row">
+        {final.map((obj, index) => {
+          console.log("objjj", obj);
+          return (
+            <>
+              <h1>name: {obj.name}</h1>
+              <h1>username: {obj.username}</h1>
+            </>
+          );
+        })}
+        </div>
+
+       
+      </div>
+    </div>
+  );
 }
 
-export default UserDetails
+export default UserDetails;
